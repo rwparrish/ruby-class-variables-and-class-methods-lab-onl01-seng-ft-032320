0db568e94f4ffa7c8990ecebc1750aab2403613a
Ryan Parrish <rino3717@gmail.com>
2020-04-01 03:29:39 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,34 @@
+class Song 
+  
+  @@count = 0
+  
+  attr_accessor :name, :artist, :genre
+  
+  def intitialize(name, artist, genre)
+    
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += count
+    
+  end
+  
+  def
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+end
